e539f10b6886312356dbf9669e4423dbd307b223
obaraelijah <elijahobara357@gmail.com>
2024-04-23 15:01:20 +0300

    feat: BTree implimentation

--- data-structures/trees/src/btree.rs
+++ data-structures/trees/src/btree.rs
@@ -1,11 +1,15 @@
 use std::collections::VecDeque;
 
+pub struct BTree {
+    root: Option<Box<Node>>,
+}
+
 struct Node {
-    numbers_of_keys: usize,
-    keys: Vec<u32>, // At least t - 1 keys, at most 2t - 1 keys
-    children: Vec<Box<Node>>, // At least t children, at most 2t children
+    numbers_of_keys: usize,    // 2t ^ h - 1.
+    keys: Vec<u32>,            // At least t - 1 keys, at most 2t - 1 keys
+    childrens: Vec<Box<Node>>, // At least t children, at most 2t children
     is_leaf: bool,
-} 
+}
 
 const MINIMUM_DEGREE: usize = 2; // t
 const MAX_DEGREE: usize = 2 * MINIMUM_DEGREE - 1;
@@ -15,52 +19,74 @@ impl Node {
         Node {
             numbers_of_keys: 0,
             keys: vec![],
-            children: Vec::new(),
+            childrens: Vec::new(),
             is_leaf,
         }
     }
 
     pub fn search(&self, key: &u32) -> Option<&u32> {
-       match self.keys.iter().position(|&k| k >= *key) {
-            Some(i) if self.keys[i] == *key => Some(&self.keys[i]),
-            Some(i) if !self.is_leaf => self.children[i].search(key),
-            _ => None,
-       }
+        let mut index = 0;
+        let mut node_key = self.keys[index];
+
+        while *key > node_key {
+            index += 1;
+
+            if index >= self.numbers_of_keys {
+                break;
+            }
+
+            node_key = self.keys[index];
+        }
+
+        if index < self.numbers_of_keys && *key == node_key {
+            return self.keys.get(index);
+        } else if self.is_leaf {
+            return None;
+        } else {
+            let next_node = &self.childrens[index];
+            return next_node.search(key);
+        }
     }
 
     pub fn split_child(&mut self, index: usize) {
-        let child = match self.children.remove(index) {
-            Some(c) => c,
-            None => return,
-        }
+        if let Some(child) = self.childrens.get_mut(index) {
+            let mut new_node = Self::new(child.is_leaf);
+            let new_node_number_of_keys = MINIMUM_DEGREE - 1;
+            new_node.numbers_of_keys = new_node_number_of_keys;
 
-        let mut new_node= Self::new(child.is_leaf);
-        new_node.numbers_of_keys = new_node_number_of_keys;
-
-        // Move keys[MINIMUM_DEGREE..] to new node
-        for _ in 0..new_node_number_of_keys {
-            let key = child.keys.remove(MINIMUM_DEGREE);
-            new_node.keys.push(key);
-        }
-
-        // Move childrens[MINIMUM_DEGREE] to new node if not leaf node
-        if !child.is_leaf {
-            for _ in 0..MINIMUM_DEGREE {
-                let node = child.children.remove(MINIMUM_DEGREE);
-                new_node.children.push(node);
+            // Move keys[t..] to new node
+            // for j = 1 to t - 1
+            //   z.key(k) = y.key(j + t)
+            // y.n = t - 1
+            for j in 0..new_node_number_of_keys {
+                let key = child.keys.remove(MINIMUM_DEGREE);
+                new_node.keys.insert(j, key);
+                child.numbers_of_keys -= 1;
             }
-        }
 
-        // x.keys(i) = y.key(t)
-        if let Some(key) = child.keys.pop() {
-            self.keys.insert(index, key)
-        }
+            // Move childrens[t..] to new node if not leaf node
+            // if not y.leaf
+            //   for j = 1 to t
+            //     z.c(j) = y.c(j+t)
+            if !child.is_leaf {
+                for j in 0..MINIMUM_DEGREE {
+                    let nodes = child.childrens.remove(MINIMUM_DEGREE);
+                    new_node.childrens.insert(j, nodes);
+                }
+            }
 
-        // x.c(i+1) = z
-        self.children.insert(index + 1, new_node);
+            // x.key(i) = y.key(t)
+            if let Some(key) = child.keys.pop() {
+                self.keys.insert(index, key);
+                child.numbers_of_keys -= 1;
+            }
 
-        // x.n = x.n + 1
-        self.numbers_of_keys += 1;
+            // x.c(i+1) = z
+            self.childrens.insert(index + 1, Box::new(new_node));
+
+            // x.n = x.n + 1
+            self.numbers_of_keys += 1;
+        };
     }
 
     pub fn insert_non_full(&mut self, key: u32) {
@@ -96,8 +122,124 @@ impl Node {
         }
     }
 
-    pub fn merge_childs() {
-        unimplimented!();
+    // Merge both child
+    //     4  |  7
+    //    /   |   \
+    //   1    6  8|9
+    //
+    // Delete 4:
+    //
+    //         7
+    //     /      \
+    //   1|4|6    8|9
+    //
+    //        7
+    //      /   \
+    //    1|6   8|9
+
+    // Get left and right child
+
+    // if key = 18
+    //     16 | 18 | 20
+    //    /   /    \   \
+    //   14  17    19  21
+    //
+    // left = 17
+    // right = 19
+    pub fn merge_childs(&mut self, index: usize) {
+        let key = self.keys.remove(index);
+        self.numbers_of_keys -= 1;
+
+        let left = self.childrens.remove(index);
+        let mut right = self.childrens.remove(index);
+
+        println!("Merging {:?}, {key}, {:?}...", left.keys, right.keys);
+
+        // Merge the keys
+        let mut new_keys = left.keys;
+        new_keys.push(key);
+        new_keys.append(&mut right.keys);
+
+        let mut left_chidrens = left.childrens;
+        let mut right_childrens = right.childrens;
+        left_chidrens.append(&mut right_childrens);
+
+        let node = Node {
+            numbers_of_keys: left.numbers_of_keys + right.numbers_of_keys + 1,
+            is_leaf: left.is_leaf,
+            keys: new_keys,
+            childrens: left_chidrens,
+        };
+
+        self.childrens.insert(index, Box::new(node));
+    }
+
+    pub fn remove_from_internals(&mut self, index: usize) -> Option<u32> {
+        let key = self.keys[index];
+
+        if self.childrens[index].numbers_of_keys >= MINIMUM_DEGREE {
+            println!("Swap with left child...");
+            //     4  |  7
+            //    /   |   \
+            //   1|2  6  8|9
+            //
+            // Delete 4:
+            //
+            //     2  |  7
+            //    /   |   \
+            //   1    6   8|9
+
+            // Recursively find the biggest left children to be swap:
+            let mut most_left = &mut self.childrens[index];
+
+            while let Some(node) = most_left.childrens.last_mut() {
+                most_left = node;
+            }
+
+            let k1 = most_left.keys[most_left.keys.len() - 1];
+
+            // Swap k1 with key:
+            println!("Replace {key} with {k1}: {:?}", self.keys);
+            let key = self.keys.remove(index);
+            self.keys.insert(index, k1);
+
+            println!("Removing {k1} from {:?}...", self.childrens[index]);
+            self.childrens[index].remove(&k1);
+
+            Some(key)
+        } else if self.childrens[index + 1].numbers_of_keys >= MINIMUM_DEGREE {
+            println!("Swap with right child...");
+            //     4  |  7
+            //    /   |   \
+            //   1   5|6  8|9
+            //
+            // Delete 4:
+            //
+            //     5  |  7
+            //    /   |   \
+            //   1    6  8|9
+            let mut most_right = &mut self.childrens[index + 1];
+
+            while let Some(node) = most_right.childrens.first_mut() {
+                most_right = node;
+            }
+
+            let k1 = most_right.keys[0];
+
+            // Swap k1 with key:
+            println!("Replace {key} with {k1}: {:?}", self.keys);
+            let key = self.keys.remove(index);
+            self.keys.insert(index, k1);
+
+            println!("Removing {k1} from {:?}...", self.childrens[index + 1]);
+            self.childrens[index + 1].remove(&k1);
+
+            Some(key)
+        } else {
+            self.merge_childs(index);
+            // Recursively call remove
+            self.childrens[index].remove(&key)
+        }
     }
 }
 
@@ -105,18 +247,32 @@ impl std::fmt::Debug for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "Node {{ keys: {:?}, numbers_of_keys: {} }}",
+            "Node {{ keys: {:?}, number_of_keys: {} }}",
             self.keys, self.numbers_of_keys
         )
     }
 }
 
-struct BTree {
-    root: Option<Box<Node>>,
-}
-
 impl BTree {
-    pub fn new() -> Self {
+    pub fn new() -> BTree {
         BTree { root: None }
+    }
+
+    pub fn insert(&mut self, key: u32) {
+        if let Some(node) = &mut self.root {
+            if node.numbers_of_keys == MAX_DEGREE {
+                let mut new_root = Node::new(false);
+                new_root.childrens.push(self.root.take().unwrap());
+                new_root.split_child(0);
+                new_root.insert_non_full(key);
+                self.root = Some(Box::new(new_root));
+            } else {
+                node.insert_non_full(key);
+            }
+        } else {
+            let mut node = Node::new(true);
+            node.insert_non_full(key);
+            self.root = Some(Box::new(node));
+        }
     }
 }
